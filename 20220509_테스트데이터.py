cfaecdb57f24ffbf3a35ab79c5c654dda2813122
jblstorm <109051965+jblstorm@users.noreply.github.com>
2022-07-11 16:28:16 +0900

    Delete 20220509_테스트데이터.py

--- 20220509_테스트데이터.py
+++
@@ -1,7 +0,0 @@
-import codecs
-
-with open("C:\\Users\\ASIA-14\Desktop\\negative_words_self.txt",encoding='utf-8') as neg:
-    negative=neg.readlines()
-negative=[neg.replace("\n","") for neg in negative]
-
-with
